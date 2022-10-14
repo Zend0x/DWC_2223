782c0604ad25b32f5c693be4b8c79a16450707c6
fernando_buendia <fbuendia@protonmail.com>
2022-10-14 17:09:37 +0200

    Clase del 14 de Octubre - Modularización del Buscaminas

--- U1/buscaminas/js/buscaminas.js
+++ U1/buscaminas/js/buscaminas.js
@@ -2,61 +2,80 @@ let columnSize = prompt("¿Cuántas columnas tiene que tener la tabla?");
 let rowSize = prompt("¿Cuántas filas tiene que tener la tabla?");
 let mineCount = prompt("¿Cuántas minas hay que insertar?");
 
-let arrayTablero = [];
-let contadorMinas = 0;
-let posFila;
-let posColumna;
+//Creamos array bidimensional e insertamos las minas.
 
-let fila = 2;
-let columna = 3;
-let minasAlrededor = 0;
-
-document.write('<table>');
-
-for (let i = 0; i < columnSize; i++) {
-    document.write('<tr>');
-    for (let j = 0; j < rowSize; j++) {
-        document.write('<td></td>');
+function crearArray(columnSize,rowSize){
+//Creación del array
+    let arrayTablero=[];
+    for (let fila = 0; fila < rowSize; fila++) {
+        arrayTablero[fila] = [];
+    
+        for (let columna = 0; columna < columnSize; columna++) {
+            arrayTablero[fila][columna] = " ";
+        }
     }
-    document.write('</tr>');
-};
-//Creamos array bidimensional para la inserción de minas
-
-for (let fila = 0; fila < rowSize; fila++) {
-    arrayTablero[fila] = [];
-
-    for (let columna = 0; columna < columnSize; columna++) {
-        arrayTablero[fila][columna] = " ";
-    }
+    return arrayTablero;
 }
 
-while (contadorMinas < mineCount) {
-    posFila = Math.floor(Math.random() * rowSize);
-    posColumna = Math.floor(Math.random() * columnSize);
-    if (arrayTablero[posFila][posColumna] != "MINA") {
-        arrayTablero[posFila][posColumna] = "MINA";
-        contadorMinas++;
-    }
-};
+function crearMinas(arrayTablero,columnSize,rowSize){
+    //Determinamos el número de minas y su posición
+    let contadorMinas=0;
+    let posFila;
+    let posColumna;
+    while (contadorMinas < mineCount) {
+        posFila = Math.floor(Math.random() * rowSize);
+        posColumna = Math.floor(Math.random() * columnSize);
+        if (arrayTablero[posFila][posColumna] != "MINA") {
+            arrayTablero[posFila][posColumna] = "MINA";
+            contadorMinas++;
+        }
+    };
+    return arrayTablero;
+}
 
-for (let fila = 0; fila < rowSize; fila++) {
-    for (let columna = 0; columna < columnSize; columna++) {
-        minasAlrededor = 0;
-        console.log("ola")
-        if (arrayTablero[fila][columna] != "MINA") {
-            for (let cFila = fila - 1; cFila < fila + 1; cFila++) {
-                for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
-                    if ((cFila >= 0 && cFila < rowSize) || (cColumna > 0 && cColumna < columnSize)) {
-                        console.log(arrayTablero[cFila][cColumna]);
-                        if (arrayTablero[cFila][cColumna] == "MINA") {
-                            minasAlrededor++;
+function mineChecker(arrayTablero,columnSize,rowSize){
+    //Determinamos las minas que hay alrededor de cada fila.
+    let minasAlrededor=0;
+    for (let fila = 0; fila < rowSize; fila++) {
+        for (let columna = 0; columna < columnSize; columna++) {
+            minasAlrededor = 0;
+            if (arrayTablero[fila][columna] != "MINA") {
+                for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                    if(cFila>=0&&cFila<rowSize){
+                        for(let cColumna=columna-1;cColumna<=columna+1;cColumna++){
+                            if((cColumna>=0&&cColumna<columnSize)&&(arrayTablero[cFila][cColumna]=="MINA")){
+                                minasAlrededor++;
+                            }
                         }
                     }
                 }
             }
+            arrayTablero[fila][columna] = minasAlrededor;
         }
-        arrayTablero[fila][columna] = minasAlrededor;
     }
+    return arrayTablero;
 }
 
-console.log(arrayTablero);
+//Creación del tablero insertando los números y minas correspondientes.
+function crearTablero(arrayTablero,columnSize,rowSize){
+    document.write('<table>');
+    for (let i = 0; i < columnSize; i++) {
+        document.write('<tr>');
+        for (let j = 0; j < rowSize; j++) {
+            if(arrayTablero[i][j]==0){
+                document.write('<td>'+arrayTablero[i][j]+'</td>');
+            }else{
+                document.write('<td>','</td>');
+            }
+        }
+        document.write('</tr>');
+    }
+    document.write('</table>')
+    return arrayTablero;
+}
+
+
+arrayTablero=crearArray(columnSize,rowSize);
+arrayTablero=crearMinas(arrayTablero,columnSize,rowSize);
+arrayTablero=mineChecker(arrayTablero,columnSize,rowSize);
+crearTablero(arrayTablero,columnSize,rowSize);
