7e96e7681078371c8f1ea24b19fb83b4c5da89bd
fernando_buendia <fbuendia@protonmail.com>
2022-10-18 19:07:49 +0200

    Clase del 18 de Octubre

--- U1/buscaminas/js/buscaminas_obj.js
+++ U1/buscaminas/js/buscaminas_obj.js
@@ -2,15 +2,16 @@ class Tablero {
     constructor(filas, columnas) {
         this.filas = filas;
         this.columnas = columnas;
-        
+
         this.crearTablero();
     }
 
     crearTablero() {
+        // Crear array bidimensional para guardar las minas
         this.arrayTablero = [];
 
         for (let fila = 0; fila < this.filas; fila++) {
-            this.arrayTablero[fila] = new Array(this.columnas);
+            this.arrayTablero[fila] = [];
 
             for (let columna = 0; columna < this.columnas; columna++) {
                 this.arrayTablero[fila][columna] = '';
@@ -18,21 +19,90 @@ class Tablero {
         }
     }
 
-    pintarTablero(){
+    dibujarTablero() {
+        // Creamos el tablero en html
         document.write('<table>');
 
-        for(let j=0;j<this.columnas;j++){
-            for(let i=0;i<this.filas;i++){
-                if(arrayTablero[i][j]==0){
-                    document.write('<td>'+arrayTablero[i][j]+'</td>');
-                }else{
-                    document.write('<td>','</td>');
+        for (let i = 0; i < this.filas; i++) {
+            document.write('<tr>');
+
+            for (let j = 0; j < this.columnas; j++) {
+                document.write(`<td>${this.arrayTablero[i][j]}</td>`);
+            }
+
+            document.write('</tr>');
+        }
+        document.write('</table>');
+    }
+
+    modificarFilas(nuevasFilas) {
+        // Modificar el número de filas y volver a crear el tablero con las filas nuevas
+        this.filas = nuevasFilas;
+
+        this.crearTablero();
+    }
+
+    modificarColumnas(nuevasColumnas) {
+        // Modificar el número de columnas y volver a crear el tablero con las columnas nuevas
+        this.columnas = nuevasColumnas;
+
+        this.crearTablero();
+    }
+
+
+}
+
+class Buscaminas extends Tablero {
+    constructor(filas, columnas, numMinas) {
+        super(filas, columnas);
+        this.numMinas = numMinas;
+
+        this.colocarMinas();
+        this.colocarNumeros();
+    }
+
+    colocarMinas() {
+        let contadorMinas = 0;
+        let posFila;
+        let posColumna;
+
+
+        while (contadorMinas < this.numMinas) {
+            posFila = Math.floor(Math.random() * this.filas);
+            posColumna = Math.floor(Math.random() * this.columnas);
+
+            if (this.arrayTablero[posFila][posColumna] != 'MINA') {
+                this.arrayTablero[posFila][posColumna] = 'MINA';
+                contadorMinas++;
+            };
+        };
+    }
+
+    colocarNumeros() {
+        let minasAlrededor = 0;
+        for (let fila = 0; fila < this.filas; fila++) {
+            for (let columna = 0; columna < this.columnas; columna++) {
+                minasAlrededor = 0;
+                if (this.arrayTablero[fila][columna] != "MINA") {
+                    for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                        if (cFila >= 0 && cFila < this.filas) {
+                            for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
+                                if ((cColumna >= 0 && cColumna < this.columnas) && (this.arrayTablero[cFila][cColumna] == "MINA")) {
+                                    minasAlrededor++;
+                                }
+                            }
+                        }
+                    }
+                    this.arrayTablero[fila][columna] = minasAlrededor;
                 }
+                
             }
         }
     }
 }
 
-const buscaminas = new Tablero(4, 3);
-buscaminas.crearTablero();
-console.log(buscaminas.arrayTablero);
+let buscaminas1 = new Buscaminas(5, 5, 5);
+
+console.log(buscaminas1.arrayTablero);
+
+buscaminas1.dibujarTablero();
