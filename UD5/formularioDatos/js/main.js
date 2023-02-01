88686faed7d9b640472cd25aff66268d09b75d7b
fernando_buendia <fbuendia@protonmail.com>
2023-02-01 20:44:39 +0100

    1 Feb - Patrones tarjetas

--- UD5/formularioDatos/js/main.js
+++ UD5/formularioDatos/js/main.js
@@ -23,6 +23,7 @@ class FormularioConJS{
         inputNombre.setAttribute('type','text');
         inputNombre.setAttribute('name','primerNombre');
         inputNombre.setAttribute('class','inputIncorrecto');
+        inputNombre.addEventListener('keyup',this.validarForm);
         nodoForm.appendChild(inputNombre);
         nodoForm.appendChild(document.createElement('br'));
         nodoForm.appendChild(document.createElement('br'));
@@ -39,12 +40,13 @@ class FormularioConJS{
         inputApellidos.setAttribute('type','text');
         inputApellidos.setAttribute('name','apellidos');
         inputApellidos.setAttribute('class','inputIncorrecto');
+        inputApellidos.addEventListener('keyup',this.validarForm);
         nodoForm.appendChild(inputApellidos);
         nodoForm.appendChild(document.createElement('br'));
         nodoForm.appendChild(document.createElement('br'));
 
         let labelEmail=document.createElement('label');
-        labelEmail.id="label";
+        labelEmail.id="labelEmail";
         labelEmail.setAttribute('for','email');
         labelEmail.innerHTML="Email";
         nodoForm.appendChild(labelEmail);
@@ -55,9 +57,29 @@ class FormularioConJS{
         inputEmail.setAttribute('type','text');
         inputEmail.setAttribute('name','email');
         inputEmail.setAttribute('class','inputIncorrecto');
+        inputEmail.addEventListener('keyup',this.validarForm);
         nodoForm.appendChild(inputEmail);
         nodoForm.appendChild(document.createElement('br'));
         nodoForm.appendChild(document.createElement('br'));
+    }
+
+    validarForm(){
+        let patron=/^[A-Za-zÁÉÍÓÚÀÈÌÒÙÖÜáéíóúàèìòùöüñÑçÇ]{2,}$/;
+
+        this.nombreClase="";
+        if(patron.test(this.value)){
+            this.nombreClase="inputBien"
+        }
+
+        let patronVisa= `[4]([0-9]{12}|[0-9]{15})`;
+        let patronMasterCard=`5[1-5][0-9]{14}`;
+        let patronDiscover=`(6011[0-9]{14})|(5[0-9]{14})`;
+        let patronAmEx=`(34|37)[0-9]{13}`;
+        let patronDiners=`(30[0-5][0-9]{11})|(36|38)[0-9]{12})`;
+        let patronJCB=`(2131|1800[0-5][0-9]{11})|(35[0-9]{14})`;
+
+        let tarjetas=`^(${patronVisa}|${patronMasterCard}|${patronDiscover}|${patronAmEx}|${patronDiners}|${patronJCB})$`;
+        let patronTarjetas=new RegExp(`${tarjetas}`);
     }
 }
 
