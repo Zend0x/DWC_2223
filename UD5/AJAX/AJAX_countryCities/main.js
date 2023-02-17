420c71d4a2d9f223786bf73e6293fddc471c2999
Zend0x <dwknerwow@gmail.com>
2023-02-17 12:51:37 +0100

    17 feb - Ejercicio 3 AJAX

--- UD5/AJAX/AJAX_countryCities/main.js
+++ UD5/AJAX/AJAX_countryCities/main.js
@@ -1,22 +1,45 @@
-function ensenarPaises(){
-    console.log("llamado");
-    letra=this.value;
-    if(letra==""){
-        document.getElementById("pais").innerHTML="";
-        return;
-    }
-    const solicitud=new XMLHttpRequest();
-    solicitud.onload=function(){
-        if(this.readyState == 4 && this.status == 200){
-            document.getElementById("pais").innerHTML=this.responseText;
+function cargarPaises() {
+    var solicitud = new XMLHttpRequest();
+    solicitud.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+            var select = document.getElementById("paises");
+            select.innerHTML=solicitud.responseText;
         }
-    }
-    solicitud.open("GET","seleccionPaises.php");
+    };
+    solicitud.open("GET", "seleccionPaises.php", true);
     solicitud.send();
-}
+  }
 
+  function cargarCiudades(paisSeleccionado){
+    paisSeleccionado=this.value;
+    var solicitud = new XMLHttpRequest();
+    solicitud.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+            var select = document.getElementById("ciudades");
+            select.innerHTML=solicitud.responseText;
+        }
+    };
+    solicitud.open("GET", "seleccionPaises.php?nombrePais="+paisSeleccionado, true);
+    solicitud.send();
+  }
+  
+  function cargarDistritos(ciudadSeleccionada){
+    ciudadSeleccionada=this.value;
+    var solicitud = new XMLHttpRequest();
+    solicitud.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+            var select = document.getElementById("distrito");
+            select.innerHTML=solicitud.responseText;
+        }
+    };
+    solicitud.open("GET", "seleccionPaises.php?nombreCiudad="+ciudadSeleccionada, true);
+    solicitud.send();
+  }
 
-window.onload = (event) => {
-    let seleccion=document.getElementById("pais");
-    ensenarPaises();
+window.onload = function() {
+    let paises=document.getElementById("paises");
+    paises.addEventListener("change",cargarCiudades);
+    let ciudades=document.getElementById("ciudades");
+    ciudades.addEventListener("change",cargarDistritos)
+    cargarPaises();
 };
--- UD5/AJAX/AJAX_countryCities/seleccionPaises.php
+++ UD5/AJAX/AJAX_countryCities/seleccionPaises.php
@@ -1,14 +1,15 @@
 <?php
-  header("access-control-allow-origin: *");
   ini_set('display_errors', 1);
   ini_set('html_errors', 1);
-  $letra = $_REQUEST['letra'];
+
+if(!isset($_GET['nombrePais'])&&!isset($_GET['nombreCiudad'])){
+  header("access-control-allow-origin: *");
   $conexion = mysqli_connect('localhost', 'root', '12345');
   if (mysqli_connect_errno()) {
     echo "Error al conectar a MySQL.";
   }
   mysqli_select_db($conexion, 'world');
-  $consulta = mysqli_prepare($conexion, 'SELECT Name FROM country;');
+  $consulta = mysqli_prepare($conexion, 'SELECT Name, Code FROM country;');
   $consulta->execute();
   $result = $consulta->get_result();
 
@@ -17,8 +18,54 @@
     array_push($resultados, $myrow);
   }
 
-  echo '<select name="seleccionPaises" id="seleccionPaises">';
+  echo '<option value="">Selecciona un país.</option>';
   foreach($resultados as $pais){
-    echo '<option value="'.$pais['Nombre'].'">'.$pais['Nombre'].'</option>';
+    echo '<option value="'.$pais['Name'].'">'.$pais['Name'].'</option>';
   }
-  echo "</select>";
+
+}else if(isset($_GET['nombrePais'])&&!empty($_GET['nombrePais'])){
+  header("access-control-allow-origin: *");
+  $paisSeleccionado=$_GET['nombrePais'];
+  $conexion = mysqli_connect('localhost', 'root', '12345');
+  if (mysqli_connect_errno()) {
+    echo "Error al conectar a MySQL.";
+  }
+  mysqli_select_db($conexion, 'world');
+  $consulta = mysqli_prepare($conexion, 'SELECT city.Name as "nombreCiudad", city.countryCode, country.Name FROM city 
+  INNER JOIN country ON countryCode=country.Code
+  WHERE country.Name = "'.$paisSeleccionado.'";');
+  $consulta->execute();
+  $result = $consulta->get_result();
+
+  $resultados = array();
+  while ($myrow = $result->fetch_assoc()) {
+    array_push($resultados, $myrow);
+  }
+  
+  echo '<option value="">Selecciona una ciudad</option>';
+  foreach($resultados as $ciudad){
+    echo '<option value="'.$ciudad['nombreCiudad'].'">'.$ciudad['nombreCiudad'].'</option>';
+  }
+}else if(isset($_GET['nombreCiudad'])&&!empty($_GET['nombreCiudad'])){
+  header("access-control-allow-origin: *");
+  $ciudadSeleccionada=$_GET['nombreCiudad'];
+  $conexion = mysqli_connect('localhost', 'root', '12345');
+  if (mysqli_connect_errno()) {
+    echo "Error al conectar a MySQL.";
+  }
+  mysqli_select_db($conexion, 'world');
+  $consulta = mysqli_prepare($conexion, 'SELECT city.Name, city.district, city.population FROM city 
+  WHERE city.Name = "'.$ciudadSeleccionada.'";');
+  $consulta->execute();
+  $result = $consulta->get_result();
+
+  $resultados = array();
+  while ($myrow = $result->fetch_assoc()) {
+    array_push($resultados, $myrow);
+  }
+  foreach($resultados as $ciudad){
+    echo '<h1>'.$ciudad['Name'].'</h1>';
+    echo '<p>Distrito: <strong>'.$ciudad['district'].'</strong></p>';
+    echo '<p>Población: <strong>'.$ciudad['population'].'</strong></p>';
+  }
+}
